c886f86e4ec805220eb0d07be4fdadbf89d950ca
Kevin Sivabalan <jbosshellokitty@gmail.com>
2013-09-05 10:38:19 -0400

    added route for picture index and controller for pictures

--- config/routes.js
+++ config/routes.js
@@ -34,7 +34,9 @@ module.exports.routes = {
   // (This would also work if you had a file at: `/views/home.ejs`)
   '/': {
     view: 'home/index'
-  }
+  },
+
+  'get /pictures': 'PictureController.index'
 
   /*
   // But what if you want your home page to display
