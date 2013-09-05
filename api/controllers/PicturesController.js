f785423da8adf486c4fce3aac145420f212f879e
Kevin Sivabalan <jbosshellokitty@gmail.com>
2013-09-05 13:04:38 -0400

    sails is being wierd

--- api/controllers/PicturesController.js
+++ api/controllers/PicturesController.js
@@ -17,26 +17,9 @@ module.exports = {
    * /pictures/index
    */ 
   index: function (req,res) {
-
-    // This will render the view: 
+	    // This will render the view: 
     // /Users/kevin/Documents/workspace/sails-photogur/views/pictures/index.ejs
-    res.view(
-    	var pictures = [
-	    		{title:"The old church on the coast of White sea",
-    			 artist:"Sergey Ershov",
-			 url:"http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-    			},
-			{title:"Sea Power",
-			 artist:"Stephen Scullion",
-			 url:"http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
-			},
-			{title:"Into the Poppies",
-			 artist:"John Wilhelm",
-			 url:"http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
-			}
-    		];
-    );
-
+  	res.view();
   }
 
 };
